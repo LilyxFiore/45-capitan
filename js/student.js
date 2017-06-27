ac6aee9ba35a89e2e507f15314f05cc172f9f371
LilyxFiore <liliana.fiorela@gmail.com>
2017-06-27 16:11:20 -0500

    subiendo html .scss

--- js/student.js
+++ js/student.js
@@ -1,3 +1,14 @@
-$.get('https://laboratoria.cuadra.co:9339/api/v1/students/', function (data) {
+function addStudents(data) {
+  $.each(data, function (i, e) {
+    $('#list').append('<li>' +e.name +'</li>');
+    console.log(e.name);
+  })
+}
+
+$.get('http://laboratoria.cuadra.co:9339/api/v1/students/', function (data) {
   console.log(data);
+  if(data != null){
+    addStudents(data);
+  }
 });
+
